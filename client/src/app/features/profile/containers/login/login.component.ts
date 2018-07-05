a32201098dbb3a3c70566631691a0989fce80d17
nomades 037 <nomades037@nomadess-iMac.local>
2018-07-05 12:02:22 +0200

    login/profile commit

--- client/src/app/features/profile/containers/login/login.component.ts
+++ client/src/app/features/profile/containers/login/login.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
   credentials: TokenPayload = {
     email: '',
     password: '',
